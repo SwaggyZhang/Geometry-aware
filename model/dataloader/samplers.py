44af5c0eed3c346bae4c54d19e33fa598ce1470c
SwaggyZhang <763461602@qq.com>
2021-12-04 20:31:26 +0800

    add new incremental sampler

--- model/dataloader/samplers.py
+++ model/dataloader/samplers.py
@@ -1,5 +1,5 @@
-import torch
 import numpy as np
+import torch
 
 
 class CategoriesSampler():
@@ -8,13 +8,16 @@ class CategoriesSampler():
         self.n_batch = n_batch
         self.n_cls = n_cls
         self.n_per = n_per
+        # 无放回抽样
+        self.cur_batch = 0
+        self.history_cls = []
 
         label = np.array(label)
         self.m_ind = []
         for i in range(max(label) + 1):
-            ind = np.argwhere(label == i).reshape(-1)
+            ind = np.argwhere(label == i).reshape(-1)  # 返回类别 i 在label集合中的索引
             ind = torch.from_numpy(ind)
-            self.m_ind.append(ind)
+            self.m_ind.append(ind)  # 各类别样本在数据集中的索引
 
     def __len__(self):
         return self.n_batch
@@ -22,7 +25,7 @@ class CategoriesSampler():
     def __iter__(self):
         for i_batch in range(self.n_batch):
             batch = []
-            classes = torch.randperm(len(self.m_ind))[:self.n_cls]
+            classes = torch.randperm(len(self.m_ind))[:self.n_cls]  # 返回一个0到len(self.m_ind)的数组，取前self.n_cls个
             for c in classes:
                 l = self.m_ind[c]
                 pos = torch.randperm(len(l))[:self.n_per]
@@ -46,8 +49,8 @@ class RandomSampler():
         for i_batch in range(self.n_batch):
             batch = torch.randperm(self.num_label)[:self.n_per]
             yield batch
-            
-            
+
+
 # sample for each class
 class ClassSampler():
 
@@ -72,12 +75,12 @@ class ClassSampler():
             else:
                 pos = torch.randperm(len(l))[:self.n_per]
             yield l[pos]
-            
-            
+
+
 # for ResNet Fine-Tune, which output the same index of task examples several times
 class InSetSampler():
 
-    def __init__(self, n_batch, n_sbatch, pool): # pool is a tensor
+    def __init__(self, n_batch, n_sbatch, pool):  # pool is a tensor
         self.n_batch = n_batch
         self.n_sbatch = n_sbatch
         self.pool = pool
@@ -89,4 +92,36 @@ class InSetSampler():
     def __iter__(self):
         for i_batch in range(self.n_batch):
             batch = self.pool[torch.randperm(self.pool_size)[:self.n_sbatch]]
+            yield batch
+
+
+class IncreSampler:
+    # TODO: 删除已经抽取过的类别
+    def __init__(self, label, n_batch, n_cls, n_per):
+        self.n_batch = n_batch
+        self.n_cls = n_cls
+        self.n_per = n_per
+        # 无放回抽样
+        self.cur_batch = 0
+        self.history_cls = []
+
+        label = np.array(label)
+        self.m_ind = []
+        for i in range(max(label) + 1):
+            ind = np.argwhere(label == i).reshape(-1)  # 返回类别 i 在label集合中的索引
+            ind = torch.from_numpy(ind)
+            self.m_ind.append(ind)  # 各类别样本在数据集中的索引
+
+    def __len__(self):
+        return self.n_batch
+
+    def __iter__(self):
+        for i_batch in range(self.n_batch):
+            batch = []
+            classes = torch.randperm(len(self.m_ind))[:self.n_cls]  # 返回一个0到len(self.m_ind)的数组，取前self.n_cls个
+            for c in classes:
+                l = self.m_ind[c]
+                pos = torch.randperm(len(l))[:self.n_per]
+                batch.append(l[pos])
+            batch = torch.stack(batch).t().reshape(-1)
             yield batch
